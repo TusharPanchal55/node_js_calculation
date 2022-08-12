cd17839c3897eeded5d00638c13006e0dadfe15b
TusharPanchal55 <109475860+TusharPanchal55@users.noreply.github.com>
2022-08-12 19:11:38 +0530

    Add files via upload

---
+++ mul.js
@@ -0,0 +1,6 @@
+var calculator = require('./calc');
+var x = 250 ,y = 10;
+console.log ("Addition ="+ calculator.add(x,y));
+console.log ("Substraction ="+ calculator.sub(x,y));
+console.log ("Multiplication ="+ calculator.mul(x,y));
+console.log ("Division ="+ calculator.div(x,y));
